8f80c13abafd48493cafb17a18ccff2574f096b4
RandDev <80163738+randwahdan@users.noreply.github.com>
2024-07-15 23:12:41 +0300

    add all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import {HeaderComponent} from  './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
+import { NgFor ,NgIf} from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [HeaderComponent, UserComponent, TasksComponent,NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser(){
+  return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id:string) {
+    this.selectedUserId= id;
+  }
 }
